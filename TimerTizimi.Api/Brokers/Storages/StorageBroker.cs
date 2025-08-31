d1be2aeb56fc13ff623db4254f69fbfb7da35e54
Elshod Ibodullayev <elshodibadullayev28@gmail.com>
2025-08-31 13:05:47 +0100

    Revert "DATA: Create TimerTizimi Model and migrate"

---
+++ TimerTizimi.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,16 @@
+﻿//===================================================
+// Copyright (c) 2025 Elshod Ibadullayev
+// Free To Use For Learning and Development
+// Project: TimerTizimi.Api
+//===================================================
+
+using Microsoft.EntityFrameworkCore;
+using TimerTizimi.Api.Models.Foundations.Timers;
+
+namespace TimerTizimi.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<TimerEntity> Timers { get; set; }
+    }
+}
