5b942f772a96a501152d0590254b5e4a1457a082
Elshod <elshodibadullayev28@gmail.com>
2025-08-31 12:05:13 +0100

    DATA: Create TimerTizimi Model and migrate

---
+++ TimerTizimi.Api/Brokers/Storages/StorageBroker.Timer.cs
@@ -0,0 +1,28 @@
+﻿//===================================================
+// Copyright (c) 2025 Elshod Ibadullayev
+// Free To Use For Learning and Development
+// Project: TimerTizimi.Api
+//===================================================
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+namespace TimerTizimi.Api.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
